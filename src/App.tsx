5538badfd54d2ebf1f2b7b0db63b32f2514b41af
Kacper <kacpersatora02@gmail.com>
2025-04-06 11:34:52 +0200

    feat: project init + pwa

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import PWABadge from "./PWABadge.tsx";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-center text-5xl">TEST</h1>
+      
+      {/* this things colors are broken */}
+      <PWABadge />
+    </>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "fit-appka",
+        short_name: "fit-appka",
+        description: "super-fitt-app",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  server: {
+    host: true,
+  },
+});
